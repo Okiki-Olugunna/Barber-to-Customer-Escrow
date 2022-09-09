f4a58d90677dfc9932d000cd406c20a3bc0f2361
Okiki-Olugunna <92333005+Okiki-Olugunna@users.noreply.github.com>
2022-09-09 20:21:03 +0100

    new deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,13 +1,42 @@
-from brownie import accounts, HairBookingEscrow
-
-
-def deploy():
-    account = accounts[0]
-    barber = accounts[1]
-    arbiter = accounts[2]
-    HairBookingEscrow.deploy(barber, arbiter, {"from": account})
-    print("Contract was successfully deployed.")
+from brownie import HairBookingEscrow, config, accounts
+import json
 
 
 def main():
-    deploy()
+    barber = accounts.add(config["wallets"]["from_key"]["main"])
+    barber_address = config["addresses"]["main_address"]
+
+    arbiter = accounts.add(config["wallets"]["from_key"]["dev_2"])
+    arbiter_address = config["addresses"]["dev_2_address"]
+
+    print("\nDeploying the contract...\n")
+
+    contract = HairBookingEscrow.deploy(
+        barber_address, arbiter_address, {"from": barber}
+    )
+
+    print("Contract has been successfully deployed.\n")
+    print(f"The contract address is {contract.address}\n")
+
+    print("Updating the abi file in the frontend...")
+    abi = contract.abi
+    json_object = json.dumps(abi)
+
+    with open("./contract_data/abi.json", "w") as file:
+        file.write(json_object)
+    with open("./frontend/utils/abi.json", "w") as file:
+        file.write(json_object)
+
+    print("Successfully updated the ABI.\n")
+
+    print("Updating the contract address file in the frontend...")
+    address = contract.address
+
+    with open("./contract_data/address.txt", "w") as file:
+        file.write(address)
+    with open("./frontend/utils/address.txt", "w") as file:
+        file.write(address)
+
+    print("Successfully updated the contract address.\n")
+
+    print("The deployed contract is ready to be interacted with. :) \n")
