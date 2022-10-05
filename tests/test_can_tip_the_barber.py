6f047ce2b8323b21c523625f971b4f2a2b5ff510
Okiki-Olugunna <92333005+Okiki-Olugunna@users.noreply.github.com>
2022-10-05 16:31:38 +0100

    test for tipping the barber

--- tests/test_can_tip_the_barber.py
+++ tests/test_can_tip_the_barber.py
@@ -1,6 +1,6 @@
-from brownie import accounts, config
+from brownie import config
 from web3 import Web3
-from decimal import Decimal
+
 
 # web3 connection
 w3 = Web3(Web3.WebsocketProvider(config["provider"]["websockets"]))
@@ -14,17 +14,16 @@ with open("./contract_data/abi.json") as file_b:
 
 contract_instance = w3.eth.contract(address=contract_address, abi=contract_abi)
 
-
-# initialising erc20 token
+# initialising WMATIC erc20 token
 with open("./contract_data/erc20_abi.json") as file_c:
     erc20_abi = file_c.read()
 
-dai_erc20_token = w3.eth.contract(
-    address="0x001B3B4d0F3714Ca98ba10F6042DaEbF0B1B7b6F", abi=erc20_abi
+wmatic_erc20_token = w3.eth.contract(
+    address="0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889", abi=erc20_abi
 )
 
 
-def test_can_tip_the_barber():
+def test_can_tip_barber():
     # initialising the accounts
     barber = config["wallets"]["from_key"]["main"]
     barber_address = config["addresses"]["main_address"]
@@ -33,19 +32,43 @@ def test_can_tip_the_barber():
     customer_1_address = config["addresses"]["dev_2_address"]
 
     # check barber's balance
-    barber_dai_balance = dai_erc20_token.functions.balanceOf(barber_address).call()
-    initial_converted_barber_balance = w3.fromWei(barber_dai_balance, "ether")
+    initial_barber_wmatic_balance = wmatic_erc20_token.functions.balanceOf(
+        barber_address
+    ).call()
 
     # check customer balance
-    customer_dai_balance = dai_erc20_token.functions.balanceOf(
+    initial_customer_wmatic_balance = wmatic_erc20_token.functions.balanceOf(
         customer_1_address
     ).call()
-    initial_converted_customer_balance = w3.fromWei(customer_dai_balance, "ether")
 
     # customer makes a tip
     tip_amount = 10
 
+    # Approving the contract to spend the customer's WMATIC...
     nonce = w3.eth.get_transaction_count(customer_1_address)
+    tx = wmatic_erc20_token.functions.approve(
+        contract_address, tip_amount
+    ).buildTransaction(
+        {
+            "gasPrice": w3.eth.gas_price,
+            "chainId": 80001,
+            "from": customer_1_address,
+            "nonce": nonce,
+        }
+    )
+    # Signing the transaction...
+    signed_tx = w3.eth.account.sign_transaction(tx, customer_1)
+    # Sending the transaction...
+    send_tx = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    # Waiting for the transaction receipt...
+    tx_receipt = w3.eth.wait_for_transaction_receipt(send_tx)
+
+    tx_receipt
+
+    # sending the tip
+    # Getting the nonce..
+    nonce = w3.eth.get_transaction_count(customer_1_address)
+    # Building the transaction...
     tx = contract_instance.functions.tip(tip_amount).buildTransaction(
         {
             "gasPrice": w3.eth.gas_price,
@@ -54,25 +77,29 @@ def test_can_tip_the_barber():
             "nonce": nonce,
         }
     )
+    # Signing the transaction...
     signed_tx = w3.eth.account.sign_transaction(tx, customer_1)
+    # Sending the transaction...
     send_tx = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
 
     # Waiting for the transaction receipt...
-
     tx_receipt = w3.eth.wait_for_transaction_receipt(send_tx)
 
-    # assertions
-    # check the new balances
+    tx_receipt
+
+    # check the new balances and assertions
     # check barber's balance
-    barber_dai_balance = dai_erc20_token.functions.balanceOf(barber_address).call()
-    new_converted_barber_balance = w3.fromWei(barber_dai_balance, "ether")
-    assert new_converted_barber_balance > initial_converted_barber_balance
+    barber_wmatic_balance = wmatic_erc20_token.functions.balanceOf(
+        barber_address
+    ).call()
+
+    assert barber_wmatic_balance > initial_barber_wmatic_balance
 
     # check customer balance
-    customer_dai_balance = dai_erc20_token.functions.balanceOf(
+    customer_wmatic_balance = wmatic_erc20_token.functions.balanceOf(
         customer_1_address
     ).call()
-    new_converted_customer_balance = w3.fromWei(customer_dai_balance, "ether")
-    assert new_converted_customer_balance < initial_converted_customer_balance
+
+    assert customer_wmatic_balance < initial_customer_wmatic_balance
 
     # end of test
